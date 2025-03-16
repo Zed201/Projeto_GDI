9c3a4e20a681ee57768d838d3256da469223af9b
Luiz Gustavo <lgpss@cin.ufpe.br>
2025-03-15 22:40:43 -0300

    Consultas.sql org

--- Projeto_Fisico/Consultas.sql
+++ Projeto_Fisico/Consultas.sql
@@ -1,6 +1,53 @@
 -- Consultando e agrupando por nome as disciplinas que possuem média de notas acima de 7
 SELECT D.NOME, AVG(A.NOTA) FROM disciplina D INNER JOIN avaliacao A ON D.ID = A.ID_DISCIPLINA GROUP BY D.NOME HAVING AVG(A.NOTA) > 7;
 
+
+-- Juncao externa, alunos ainda sem prova e com inner join para ver o nome deles
+SELECT AL.ID, AL.NOME FROM (
+		SELECT IA.ID, P.NOME FROM ALUNO IA INNER JOIN PESSOA P ON P.ID = IA.ID
+	) AL LEFT OUTER JOIN AVALIACAO AV ON AV.ID_ALUNO = AL.ID WHERE AV.ID IS NULL;
+
+-- Semi Juncao, selecionar as disciplinas com avaliacao
+SELECT * FROM DISCIPLINA D WHERE D.ID IN (SELECT A.ID_DISCIPLINA FROM AVALIACAO A);
+
+-- Retorna a quantidade de projetos por professor
+SELECT P.ID, P.NOME, COUNT(S.ID_PROFESSOR) AS NUMERO_DE_PROJETOS FROM 
+PESSOA P INNER JOIN ASSUME S ON P.ID = S.ID_PROFESSOR
+GROUP BY P.NOME, P.ID; 
+
+-- Subconsulta do tipo escalar
+SELECT MAX(DT_MATRICULA) AS MATRICULA_RECENTE FROM MATRICULA;
+
+-- Subconsulta do tipo tabela
+SELECT A.ID_ALUNO,(SELECT NOME FROM PESSOA WHERE ID = A.ID_ALUNO) AS NOME, A.ID_DISCIPLINA, A.NOTA FROM AVALIACAO A JOIN (
+    SELECT ID_DISCIPLINA, MAX(NOTA) AS MAX_NOTA FROM AVALIACAO GROUP BY ID_DISCIPLINA
+) ALIAS ON A.ID_DISCIPLINA = ALIAS.ID_DISCIPLINA AND A.NOTA = ALIAS.MAX_NOTA;
+
+-- Anti Join (Saber quais as disciplinas sem avaliações)
+SELECT D.nome
+FROM disciplina D
+WHERE D.id NOT IN(SELECT A.id_disciplina FROM avaliacao A WHERE A.id_disciplina IS NOT NULL)
+
+------------------------------------------------------------------------ PL
+
+-- Procedure de Subconsulta tipo linha (Saber os id dos coordenadores que tem o mesmo departamento e curso)
+CREATE OR REPLACE PROCEDURE coord_dep_e_curso (coord_id INTEGER) IS
+    CURSOR cur_coordenadores IS
+        SELECT C.ID
+        FROM coordenador C
+        WHERE (C.departamento, C.nome_curso) = 
+              (SELECT C2.departamento, C2.nome_curso
+               FROM coordenador C2
+               WHERE C2.ID = coord_id);
+    
+BEGIN
+    FOR rec IN cur_coordenadores LOOP
+        DBMS_OUTPUT.PUT_LINE('ID: ' || rec.ID);
+    END LOOP;
+END;
+
+exec coord_dep_e_curso(3); -- Retorna so o id 3
+
 -- Consulta os códigos dos projetos que um determinado professor ocupa com um determinado cargo
 CREATE OR REPLACE PROCEDURE projetos_prof_cargo (prof_id INTEGER, cargo_cod INTEGER) IS
 		CURSOR cur_proj IS SELECT codigo_projeto FROM assume where codigo_cargo = cargo_cod AND id_professor = prof_id;
@@ -15,46 +62,8 @@ CREATE OR REPLACE PROCEDURE projetos_prof_cargo (prof_id INTEGER, cargo_cod INTE
 	END;
 
 -- Apenas executa o procedimento acima para 1,1 deve mostrar no console 101 e para 2,2 deve mostrar 102
+EXEC projetos_prof_cargo(1, 1);
 EXEC projetos_prof_cargo(2, 2);
-
--- Juncao externa, alunos ainda sem prova e com inner join para ver o nome deles
-SELECT AL.ID, AL.NOME FROM (
-		SELECT IA.ID, P.NOME FROM ALUNO IA INNER JOIN PESSOA P ON P.ID = IA.ID
-	) AL LEFT OUTER JOIN AVALIACAO AV ON AV.ID_ALUNO = AL.ID WHERE AV.ID IS NULL;
-
-
--- Semi Juncao, selecionar as disciplinas com avaliacao
-SELECT * FROM DISCIPLINA D WHERE D.ID IN (SELECT A.ID_DISCIPLINA FROM AVALIACAO A);
-
-
--- Trigger de mundaça de professor que ensina disciplina
-CREATE OR REPLACE TRIGGER Novo_prof_disciplina
-BEFORE UPDATE OF ID_PROFESSOR ON ENSINA 
-FOR EACH ROW
-DECLARE
-    DI DISCIPLINA.NOME%TYPE;
-    NOME PESSOA.NOME%TYPE;
-BEGIN
-    SELECT D.NOME INTO DI FROM DISCIPLINA D WHERE D.ID = :NEW.ID_DISCIPLINA;
-    SELECT P.NOME INTO NOME FROM PESSOA P WHERE :NEW.ID_PROFESSOR = P.ID;
-    IF (:NEW.ID_PROFESSOR <> :OLD.ID_PROFESSOR) THEN
-        DBMS_OUTPUT.PUT_LINE('Disciplina de ' || DI || ' com novo prof ' || NOME );
-    END IF;
-END;
-
-
--- Retorna a quantidade de projetos por professor
-SELECT P.ID, P.NOME, COUNT(S.ID_PROFESSOR) AS NUMERO_DE_PROJETOS FROM 
-PESSOA P INNER JOIN ASSUME S ON P.ID = S.ID_PROFESSOR
-GROUP BY P.NOME, P.ID; 
-
--- Subconsulta do tipo escalar
-SELECT MAX(DT_MATRICULA) AS MATRICULA_RECENTE FROM MATRICULA;
-
--- Subconsulta do tipo tabela
-SELECT A.ID_ALUNO,(SELECT NOME FROM PESSOA WHERE ID = A.ID_ALUNO) AS NOME, A.ID_DISCIPLINA, A.NOTA FROM AVALIACAO A JOIN (
-    SELECT ID_DISCIPLINA, MAX(NOTA) AS MAX_NOTA FROM AVALIACAO GROUP BY ID_DISCIPLINA
-) ALIAS ON A.ID_DISCIPLINA = ALIAS.ID_DISCIPLINA AND A.NOTA = ALIAS.MAX_NOTA;
 
 -- Atualiza os Valores de requisito
 CREATE OR REPLACE PROCEDURE atualiza_requisito(
@@ -79,24 +88,17 @@ BEGIN
     END IF;
 END atualiza_requisito;
 
--- Anti Join (Saber quais as disciplinas sem avaliações)
-SELECT D.nome
-FROM disciplina D
-WHERE D.id NOT IN(SELECT A.id_disciplina FROM avaliacao A WHERE A.id_disciplina IS NOT NULL)
-
--- Procedure de Subconsulta tipo linha (Saber os id dos coordenadores que tem o mesmo departamento e curso)
-CREATE OR REPLACE PROCEDURE coord_dep_e_curso (coord_id INTEGER) IS
-    CURSOR cur_coordenadores IS
-        SELECT C.ID
-        FROM coordenador C
-        WHERE (C.departamento, C.nome_curso) = 
-              (SELECT C2.departamento, C2.nome_curso
-               FROM coordenador C2
-               WHERE C2.ID = coord_id);
-    
+-- Trigger de mundaça de professor que ensina disciplina
+CREATE OR REPLACE TRIGGER Novo_prof_disciplina
+BEFORE UPDATE OF ID_PROFESSOR ON ENSINA 
+FOR EACH ROW
+DECLARE
+    DI DISCIPLINA.NOME%TYPE;
+    NOME PESSOA.NOME%TYPE;
 BEGIN
-    FOR rec IN cur_coordenadores LOOP
-        DBMS_OUTPUT.PUT_LINE('ID: ' || rec.ID);
-    END LOOP;
+    SELECT D.NOME INTO DI FROM DISCIPLINA D WHERE D.ID = :NEW.ID_DISCIPLINA;
+    SELECT P.NOME INTO NOME FROM PESSOA P WHERE :NEW.ID_PROFESSOR = P.ID;
+    IF (:NEW.ID_PROFESSOR <> :OLD.ID_PROFESSOR) THEN
+        DBMS_OUTPUT.PUT_LINE('Disciplina de ' || DI || ' com novo prof ' || NOME );
+    END IF;
 END;
-/
